0ea7d96ef54d9bfc6f5e258cd00f6b5dff9064e7
Bent <bent@tjukkas.no>
2018-01-17 22:29:38 +0100

    Made setupscript for Ubuntu

---
+++ ubuntu_setup.sh
@@ -0,0 +1,34 @@
+if [ "$EUID" -ne 0 ]
+    then echo "Must be run as root"
+    exit
+fi
+apt update
+apt upgrade
+apt install git
+apt install terminator
+snap install spotify
+apt install vim
+apt install openssh-server
+apt install build-essential cmake
+apt install python-dev python3-dev
+apt install chromium-browser
+
+echo Git email
+read email
+echo Git name
+read name
+
+git config --global user.email $email
+git config --global user.name  $name
+
+#moves RCs
+cp bashrc ~/.bashrc
+cp vimrc ~/.vimrc
+
+#installs vim plugins
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone https://github.com/Valloric/YouCompleteMe.git
+cd ~/.vim/bundle/YouCompleteMe
+./install.py --clang-completer
+#starts vim with the intall command
+vim +BundleInstall
